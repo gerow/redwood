a8b2940d7903b0b1cff98f4b47a0f10ff16aeaba
gerow <gerow.mike@gmail.com>
2015-10-01 21:22:02 -0700

    We have a kernel! It halts right after it starts!

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	$(MAKE) -C cross
+	$(MAKE) -C kern
 
 clean:
 	$(MAKE) -C cross clean
---
+++ kern/start.s
@@ -0,0 +1,26 @@
+.set ALIGN, 1 << 0
+.set MEMINFO, 1 << 1
+.set FLAGS, ALIGN | MEMINFO
+.set MAGIC, 0x1badb002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bootstrap_stack, "aw", @nobits
+stack_bottom:
+.skip 16384
+stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+  movl $stack_top, %esp
+  cli
+  hlt
+.Lloop:
+  jmp .Lloop
